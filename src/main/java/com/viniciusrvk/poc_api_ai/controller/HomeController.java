548ed7ea4603698426d0a5e0aaea64efebca61e0
Vinicius Kuo <roberto.kuo@luizalabs.com>
2025-04-21 17:18:52 -0300

    feat: index page to test the chat

---
+++ src/main/java/com/viniciusrvk/poc_api_ai/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.viniciusrvk.poc_api_ai.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/index.html";
+    }
+}
