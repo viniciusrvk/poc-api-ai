e6ed6f86e695b7ac39b64f763a05dee877c8fcad
Vinicius Kuo <roberto.kuo@luizalabs.com>
2025-04-21 14:40:17 -0300

    chore: inicio poc

---
+++ src/main/java/com/viniciusrvk/poc_api_ai/PocApiAiApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusrvk.poc_api_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocApiAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocApiAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciusrvk/poc_api_ai/PocApiAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusrvk.poc_api_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApiAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
